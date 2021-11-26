62d1317ae6c990e66e06f547517740e884e6375e
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2021-11-26 11:19:45 -0600

    add chores worker and trigger, actually put lambda functions in terraform plan

---
+++ cmd/workers/lambdas/chores/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+
+	"github.com/prixfixeco/api_server/internal/config"
+	customerdataconfig "github.com/prixfixeco/api_server/internal/customerdata/config"
+	"github.com/prixfixeco/api_server/internal/database/queriers/postgres"
+	emailconfig "github.com/prixfixeco/api_server/internal/email/config"
+	msgconfig "github.com/prixfixeco/api_server/internal/messagequeue/config"
+	"github.com/prixfixeco/api_server/internal/observability"
+	"github.com/prixfixeco/api_server/internal/observability/logging"
+	"github.com/prixfixeco/api_server/internal/workers"
+)
+
+func buildHandler(worker *workers.ChoresWorker) func(ctx context.Context, sqsEvent events.SQSEvent) error {
+	return func(ctx context.Context, sqsEvent events.SQSEvent) error {
+		for i := 0; i < len(sqsEvent.Records); i++ {
+			message := sqsEvent.Records[i]
+			if err := worker.HandleMessage(ctx, []byte(message.Body)); err != nil {
+				return observability.PrepareError(err, nil, nil, "handling writes message")
+			}
+		}
+
+		return nil
+	}
+}
+
+func main() {
+	ctx := context.Background()
+	logger := logging.NewZerologLogger()
+	client := &http.Client{Timeout: 10 * time.Second}
+
+	cfg, err := config.GetConfigFromParameterStore()
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	dataManager, err := postgres.ProvideDatabaseClient(ctx, logger, &cfg.Database)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	publisherProvider, err := msgconfig.ProvidePublisherProvider(logger, &cfg.Events)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	postChoresPublisher, err := publisherProvider.ProviderPublisher("data_changes")
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	emailer, err := emailconfig.ProvideEmailer(&cfg.Email, logger, client)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	cdp, err := customerdataconfig.ProvideCollector(&cfg.CustomerData, logger)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	preChoresWorker := workers.ProvideChoresWorker(
+		logger,
+		dataManager,
+		postChoresPublisher,
+		emailer,
+		cdp,
+	)
+
+	lambda.Start(buildHandler(preChoresWorker))
+}
--- environments/dev/terraform/workers.tf
+++ environments/dev/terraform/workers.tf
@@ -33,6 +33,10 @@ resource "aws_lambda_function" "writes_worker_lambda" {
   memory_size   = local.memory_size
   timeout       = local.timeout
 
+  filename = "writer_lambda.zip"
+  source_code_hash = filebase64sha256("writer_lambda.zip")
+
+
   tags = {
     Environment = "dev"
     Terraform   = "true"
@@ -46,6 +50,9 @@ resource "aws_lambda_function" "updates_worker_lambda" {
   runtime       = local.lambda_runtime
   memory_size   = local.memory_size
   timeout       = local.timeout
+
+  filename = "updater_lambda.zip"
+  source_code_hash = filebase64sha256("updater_lambda.zip")
 
   tags = {
     Environment = "dev"
@@ -61,6 +68,9 @@ resource "aws_lambda_function" "archives_worker_lambda" {
   memory_size   = local.memory_size
   timeout       = local.timeout
 
+  filename = "archiver_lambda.zip"
+  source_code_hash = filebase64sha256("archiver_lambda.zip")
+
   tags = {
     Environment = "dev"
     Terraform   = "true"
@@ -75,8 +85,40 @@ resource "aws_lambda_function" "data_changes_worker_lambda" {
   memory_size   = local.memory_size
   timeout       = local.timeout
 
+  filename = "data_changes_lambda.zip"
+  source_code_hash = filebase64sha256("data_changes_lambda.zip")
+
   tags = {
     Environment = "dev"
     Terraform   = "true"
   }
+}
+
+resource "aws_lambda_function" "chores_worker_lambda" {
+  function_name = "chores_worker"
+  role          = aws_iam_role.worker_lambda_role.arn
+  handler       = local.lambda_handler
+  runtime       = local.lambda_runtime
+  memory_size   = local.memory_size
+  timeout       = local.timeout
+
+  filename = "chores_lambda.zip"
+  source_code_hash = filebase64sha256("chores_lambda.zip")
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
+
+resource "aws_cloudwatch_event_rule" "every_minute" {
+    name = "every-minute"
+    description = "Fires every minute"
+    schedule_expression = "rate(1 minute)"
+}
+
+resource "aws_cloudwatch_event_target" "run_chores_every_minute" {
+    rule = "${aws_cloudwatch_event_rule.every_minute.name}"
+    target_id = "chores_worker"
+    arn = "${aws_lambda_function.chores_worker_lambda.arn}"
 }
