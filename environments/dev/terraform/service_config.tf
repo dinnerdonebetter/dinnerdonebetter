0101f21e1baf3fe0d6417faa96ab602918a6fcca
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2021-11-26 20:13:59 -0600

    here we go again 6

--- environments/dev/terraform/service_config.tf
+++ environments/dev/terraform/service_config.tf
@@ -1,5 +1,9 @@
+data "local_file" "service_config" {
+  filename = "${path.module}/../config_files/service-config.json"
+}
+
 resource "aws_ssm_parameter" "service_config" {
   name  = "PRIXFIXE_BASE_CONFIG"
   type  = "String"
-  value = file("../config_files/service-config.json")
+  value = data.local_file.service_config.content
 }
