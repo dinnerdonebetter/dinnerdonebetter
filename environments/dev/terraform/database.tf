d028757185bc184ebf1acfada1e023462a58a8d6
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2021-11-27 13:38:56 -0600

    i have no fucking idea what i'm doing #1

--- environments/dev/terraform/database.tf
+++ environments/dev/terraform/database.tf
@@ -29,6 +29,9 @@ resource "aws_rds_cluster" "api_database" {
   storage_encrypted       = true
   preferred_backup_window = "01:00-05:00"
 
+  enable_http_endpoint = true
+  vpc_security_group_ids = [ aws_security_group.allow_postgres.id ]
+
   tags = merge(var.default_tags, {})
 }
 
---
+++ environments/dev/terraform/security_groups.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "allow_postgres" {
+  name        = "allow_postgres"
+  description = "Allow Postgres inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "Postgres"
+    from_port        = 5432
+    to_port          = 5432
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = merge(var.default_tags, {})
+}
