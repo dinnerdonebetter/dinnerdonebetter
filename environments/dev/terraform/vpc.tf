6647cca2394547039e1cc58c0068e28d36dc7b38
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2021-11-27 20:46:01 -0600

    trim down inventory for Fargate testing #7

--- environments/dev/terraform/vpc.tf
+++ environments/dev/terraform/vpc.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.0.0/24"
 
   tags = merge(var.default_tags, {})
 }
