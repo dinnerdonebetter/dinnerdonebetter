6fc5a129fa54c2fd559771d2241ddab9b5e8e2e8
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2021-11-27 22:07:44 -0600

    trim down inventory for Fargate testing #14

--- environments/dev/terraform/vpc.tf
+++ environments/dev/terraform/vpc.tf
@@ -8,20 +8,20 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "east1a" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.0.0/24"
+  cidr_block        = "10.0.1.0/26"
   availability_zone = "us-east-1a"
 
   tags = merge(var.default_tags, {
-    Name = "main"
+    Name = "main-east1a"
   })
 }
 
 resource "aws_subnet" "east1b" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.0.0/32"
+  cidr_block        = "10.0.1.64/26"
   availability_zone = "us-east-1b"
 
   tags = merge(var.default_tags, {
-    Name = "main"
+    Name = "main-east1b"
   })
 }
