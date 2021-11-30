f9f03221159a5bae39eb9bb3d6947074f16b8001
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2021-11-30 00:15:34 -0600

    let's do this 25

--- cmd/debug/main.go
+++ cmd/debug/main.go
@@ -8,10 +8,11 @@ import (
 
 func main() {
 	http.HandleFunc("/", handler)
-	fmt.Println("Server started at port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Println("Server started at port 80")
+	log.Fatal(http.ListenAndServe(":80", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	log.Println("handler invoked")
 	fmt.Fprintf(w, "Hello, there\n")
 }
