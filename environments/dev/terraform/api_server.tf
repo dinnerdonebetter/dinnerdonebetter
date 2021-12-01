55b3f4fea934c611993deceab14a0a22e9ffc0f8
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2021-11-30 20:41:02 -0600

    here we fuckin' go again 9

--- environments/dev/terraform/api_server.tf
+++ environments/dev/terraform/api_server.tf
@@ -3,8 +3,8 @@ locals {
 }
 
 resource "aws_ecr_repository" "api_server" {
-  name                 = "api_server"
-  image_tag_mutability = "IMMUTABLE"
+  name = "api_server"
+  # image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
