0cfa94c9b3c1dffd934c0fa33fdbde975bf8aab8
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2021-12-02 19:18:07 -0600

    trying once more 7

--- environments/dev/terraform/network.tf
+++ environments/dev/terraform/network.tf
@@ -89,6 +89,11 @@ resource "aws_route_table" "private" {
   }
 }
 
+resource "aws_main_route_table_association" "main" {
+  vpc_id         = aws_vpc.main.id
+  route_table_id = aws_route_table.private.id
+}
+
 resource "aws_route" "public_igw" {
   route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
