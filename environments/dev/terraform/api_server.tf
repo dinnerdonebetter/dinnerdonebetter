eb34c45912503df174fb1fc1d3c05b9737375154
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2021-12-03 21:39:19 -0600

    once more 'round the bend 14

--- environments/dev/terraform/api_server.tf
+++ environments/dev/terraform/api_server.tf
@@ -122,12 +122,13 @@ data "aws_iam_policy_document" "ecs_task_assume_role" {
       "sqs:ReceiveMessage",
     ]
 
+    resources = [
+      aws_sqs_queue.writes_queue.arn,
+    ]
+
     principals {
       type        = "Service"
       identifiers = ["ecs-tasks.amazonaws.com"]
-      resources = [
-        aws_sqs_queue.writes_queue.arn,
-      ]
     }
   }
 }
