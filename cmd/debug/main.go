4d11768bbef9150fadd16d03a163b13f1d73bda8
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2021-12-03 20:15:03 -0600

    once more 'round the bend 5

--- cmd/debug/main.go
+++ cmd/debug/main.go
@@ -6,15 +6,21 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
 	_ "github.com/lib/pq"
 )
 
 const (
-	dbURL = "postgres://prixfixe_api:password@api-database.cluster-ctj4wxgujo7g.us-east-1.rds.amazonaws.com:5432/prixfixe"
+	dbURL    = "postgres://prixfixe_api:password@api-database.cluster-ctj4wxgujo7g.us-east-1.rds.amazonaws.com:5432/prixfixe"
+	queueURL = "https://sqs.us-east-1.amazonaws.com/966107642521/writes.fifo"
+	// queueArn = "arn:aws:sqs:us-east-1:966107642521:writes.fifo"
 )
 
 var (
-	db *sql.DB
+	db    *sql.DB
+	queue *sqs.SQS
 )
 
 func main() {
@@ -27,6 +33,11 @@ func main() {
 		log.Fatalf("error connecting to database: %v", err)
 	}
 
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+	queue = sqs.New(sess)
+
 	log.Fatal(http.ListenAndServe(":80", nil))
 }
 
@@ -37,6 +48,20 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	row := db.QueryRow("SELECT VERSION()")
 	if err := row.Scan(&version); err != nil {
 		log.Printf("error querying database: %v", err)
+		fmt.Fprintf(w, "Error: %s", err)
+	}
+
+	if _, err := queue.SendMessage(&sqs.SendMessageInput{
+		MessageAttributes: map[string]*sqs.MessageAttributeValue{
+			"Things": {
+				DataType:    aws.String("String"),
+				StringValue: aws.String("Stuff"),
+			},
+		},
+		MessageBody: aws.String("just testin'"),
+		QueueUrl:    aws.String(queueURL),
+	}); err != nil {
+		log.Printf("error writing message to queue: %v", err)
 		fmt.Fprintf(w, "Error: %s", err)
 	}
 
