a219991bcd43c100db45bf283fc567ffb3b279fb
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2021-12-04 14:01:23 -0600

    Revert "once more 'round the bend 24"
    This reverts commit 242219c0e913cef212c223cbf278f74dfea8abe3.

--- environments/dev/terraform/workers.tf
+++ environments/dev/terraform/workers.tf
@@ -5,15 +5,6 @@ locals {
   timeout        = 30
 }
 
-resource "aws_ecr_repository" "writer" {
-  name = "writer"
-  # do not set image_tag_mutability to "IMMUTABLE", or else we cannot use :latest tags.
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
-
 resource "aws_lambda_function" "writes_worker_lambda" {
   function_name = "writes_worker"
   role          = aws_iam_role.worker_lambda_role.arn
@@ -22,16 +13,8 @@ resource "aws_lambda_function" "writes_worker_lambda" {
   memory_size   = local.memory_size
   timeout       = local.timeout
 
-  image_uri = aws_ecr_repository.writer.repository_url
-}
-
-resource "aws_ecr_repository" "updater" {
-  name = "updater"
-  # do not set image_tag_mutability to "IMMUTABLE", or else we cannot use :latest tags.
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
+  # filename         = "writer_lambda.zip"
+  # source_code_hash = filebase64sha256("writer_lambda.zip")
 }
 
 resource "aws_lambda_function" "updates_worker_lambda" {
@@ -42,16 +25,8 @@ resource "aws_lambda_function" "updates_worker_lambda" {
   memory_size   = local.memory_size
   timeout       = local.timeout
 
-  image_uri = aws_ecr_repository.updater.repository_url
-}
-
-resource "aws_ecr_repository" "archiver" {
-  name = "archiver"
-  # do not set image_tag_mutability to "IMMUTABLE", or else we cannot use :latest tags.
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
+  # filename         = "updater_lambda.zip"
+  # source_code_hash = filebase64sha256("updater_lambda.zip")
 }
 
 resource "aws_lambda_function" "archives_worker_lambda" {
@@ -62,16 +37,8 @@ resource "aws_lambda_function" "archives_worker_lambda" {
   memory_size   = local.memory_size
   timeout       = local.timeout
 
-  image_uri = aws_ecr_repository.archiver.repository_url
-}
-
-resource "aws_ecr_repository" "data_change_observer" {
-  name = "data_change_observer"
-  # do not set image_tag_mutability to "IMMUTABLE", or else we cannot use :latest tags.
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
+  # filename         = "archiver_lambda.zip"
+  # source_code_hash = filebase64sha256("archiver_lambda.zip")
 }
 
 resource "aws_lambda_function" "data_changes_worker_lambda" {
@@ -82,16 +49,8 @@ resource "aws_lambda_function" "data_changes_worker_lambda" {
   memory_size   = local.memory_size
   timeout       = local.timeout
 
-  image_uri = aws_ecr_repository.data_change_observer.repository_url
-}
-
-resource "aws_ecr_repository" "chore_worker" {
-  name = "chore_worker"
-  # do not set image_tag_mutability to "IMMUTABLE", or else we cannot use :latest tags.
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
+  # filename         = "data_changes_lambda.zip"
+  # source_code_hash = filebase64sha256("data_changes_lambda.zip")
 }
 
 resource "aws_lambda_function" "chores_worker_lambda" {
@@ -102,7 +61,8 @@ resource "aws_lambda_function" "chores_worker_lambda" {
   memory_size   = local.memory_size
   timeout       = local.timeout
 
-  image_uri = aws_ecr_repository.chore_worker.repository_url
+  # filename         = "chores_lambda.zip"
+  # source_code_hash = filebase64sha256("chores_lambda.zip")
 }
 
 resource "aws_cloudwatch_event_rule" "every_minute" {
