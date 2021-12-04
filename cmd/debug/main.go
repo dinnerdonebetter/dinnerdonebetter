ee075c762ef30eaae445d5c66965d42be4c46f7b
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2021-12-03 19:50:39 -0600

    once more 'round the bend 2

--- cmd/debug/main.go
+++ cmd/debug/main.go
@@ -1,35 +1,29 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 )
 
-/*
-CREATE TABLE IF NOT EXISTS stuff (
-	"name" TEXT NOT NULL,
-	"description" TEXT NOT NULL
-);
-*/
-
 const (
-// dbURL = "postgres://prixfixe_api"
+	dbURL = "postgres://prixfixe_api:password@api-database.cluster-ctj4wxgujo7g.us-east-1.rds.amazonaws.com:5432/prixfixe"
 )
 
-// var (
-// 	db *sql.DB
-// )
+var (
+	db *sql.DB
+)
 
 func main() {
 	http.HandleFunc("/", handler)
 	fmt.Println("Server started at port 80")
 
-	// var err error
-	// db, err = sql.Open("postgres", dbURL)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	var err error
+	db, err = sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("error connecting to database: %v", err)
+	}
 
 	log.Fatal(http.ListenAndServe(":80", nil))
 }
@@ -37,8 +31,12 @@ func main() {
 func handler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("handler invoked for request: %s %s", r.Method, r.URL.String())
 
-	// age := 21
-	// rows, err := db.Query("SELECT  FROM users WHERE age = $1", age)
+	var version string
+	row := db.QueryRow("SELECT VERSION()")
+	if err := row.Scan(&version); err != nil {
+		log.Printf("error querying database: %v", err)
+		fmt.Fprintf(w, "Error: %s", err)
+	}
 
-	fmt.Fprintf(w, "Hello, there!")
+	fmt.Fprintf(w, "Hello, there from version %s!", version)
 }
