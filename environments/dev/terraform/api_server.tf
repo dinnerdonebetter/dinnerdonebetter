4ed169bed8fe63ab21c843a768e5e972cfcc7bd9
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2021-12-05 13:30:24 -0600

    change service port for real

--- environments/dev/terraform/api_server.tf
+++ environments/dev/terraform/api_server.tf
@@ -118,10 +118,10 @@ data "aws_iam_policy_document" "ecs_task_assume_role" {
 }
 
 resource "aws_iam_role" "api_task_role" {
-  name               = "api-task-role"
-  
+  name = "api-task-role"
+
   assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role.json
-  
+
   managed_policy_arns = [
     aws_iam_policy.api_service_policy.arn,
   ]
