2adaca7735cd9b65e0f03e453e5a78d2260c4ac2
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2021-12-05 12:57:38 -0600

    make server roles reusable

--- environments/dev/dockerfiles/api_server.Dockerfile
+++ environments/dev/dockerfiles/api_server.Dockerfile
@@ -5,7 +5,7 @@ WORKDIR /go/src/github.com/prixfixeco/api_server
 
 COPY . .
 
-RUN go build -trimpath -o /server -v github.com/prixfixeco/api_server/cmd/debug/server
+RUN go build -trimpath -o /server -v github.com/prixfixeco/api_server/cmd/server
 
 # final stage
 FROM debian:stretch
--- environments/dev/terraform/iam.tf
+++ environments/dev/terraform/iam.tf
@@ -1,51 +1,51 @@
+data "aws_iam_policy_document" "allow_to_read_from_queues" {
+  statement {
+    effect = "Allow"
+    actions   = [
+      "sqs:*",
+    ]
+    resources =  [
+      aws_sqs_queue.writes_queue.arn,
+      aws_sqs_queue.updates_queue.arn,
+      aws_sqs_queue.archives_queue.arn,
+      aws_sqs_queue.data_changes_queue.arn,
+      aws_sqs_queue.chores_queue.arn,
+    ],
+  }
+}
+
+data "aws_iam_policy_document" "allow_parameter_store_access" {
+  statement {
+    effect = "Allow"
+    actions   = [
+      "ssm:GetParameter",
+      "ssm:GetParameters",
+      "ssm:DescribeParameters",
+      "ssm:GetParametersByPath",
+    ]
+    resources =  [
+      aws_ssm_parameter.service_config.arn,
+      aws_ssm_parameter.writes_queue_parameter.arn,
+      aws_ssm_parameter.updates_queue_parameter.arn,
+      aws_ssm_parameter.archives_queue_parameter.arn,
+      aws_ssm_parameter.data_changes_queue_parameter.arn,
+      aws_ssm_parameter.chores_queue_parameter.arn,
+      aws_ssm_parameter.database_url.arn,
+    ],
+  }
+}
+
 resource "aws_iam_role" "worker_lambda_role" {
   name = "Worker"
 
   inline_policy {
     name = "allow_sqs_queue_access"
-
-    policy = jsonencode({
-      Version = "2012-10-17",
-      Statement = [
-        {
-          Action = [
-            "sqs:*",
-          ],
-          Effect = "Allow",
-          Resource = [
-            aws_sqs_queue.writes_queue.arn,
-            aws_sqs_queue.updates_queue.arn,
-            aws_sqs_queue.archives_queue.arn,
-            aws_sqs_queue.data_changes_queue.arn,
-            aws_sqs_queue.chores_queue.arn,
-          ],
-        }
-      ]
-    })
+    policy = aws_iam_policy_document.allow_to_read_from_queues.json
   }
 
   inline_policy {
     name = "allow_ssm_access"
-    policy = jsonencode({
-      Version = "2012-10-17",
-      Statement = [
-        {
-          Action = [
-            "ssm:GetParameter",
-          ],
-          Effect = "Allow",
-          Resource = [
-            aws_ssm_parameter.service_config.arn,
-            aws_ssm_parameter.writes_queue_parameter.arn,
-            aws_ssm_parameter.updates_queue_parameter.arn,
-            aws_ssm_parameter.archives_queue_parameter.arn,
-            aws_ssm_parameter.data_changes_queue_parameter.arn,
-            aws_ssm_parameter.chores_queue_parameter.arn,
-            aws_ssm_parameter.database_url.arn,
-          ],
-        }
-      ]
-    })
+    policy = aws_iam_policy_document.allow_parameter_store_access.json
   }
 
   managed_policy_arns = [
@@ -71,19 +71,12 @@ resource "aws_iam_role" "server_lambda_role" {
 
   inline_policy {
     name = "allow_sqs_queue_access"
+    policy = aws_iam_policy_document.allow_to_read_from_queues.json
+  }
 
-    policy = jsonencode({
-      Version = "2012-10-17",
-      Statement = [
-        {
-          Action = [
-            "sqs:*",
-          ],
-          Effect   = "Allow",
-          Resource = "*",
-        },
-      ],
-    })
+  inline_policy {
+    name = "allow_ssm_access"
+    policy = aws_iam_policy_document.allow_parameter_store_access.json
   }
 
   assume_role_policy = jsonencode({
