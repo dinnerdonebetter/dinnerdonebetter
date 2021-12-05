89ac1fdf1306477e21cd327359b94ab058dcc9f5
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2021-12-04 19:36:29 -0600

    add inline policy to worker role

--- environments/dev/terraform/iam.tf
+++ environments/dev/terraform/iam.tf
@@ -1,29 +1,33 @@
 resource "aws_iam_role" "worker_lambda_role" {
   name = "Worker"
 
+  inline_policy {
+    name = "allow_sqs_queue_access"
+
+    policy = jsonencode({
+      Version = "2012-10-17",
+      Statement = [
+        {
+          Action = [
+            "sqs:SendMessage",
+            "sqs:ReceiveMessage",
+          ],
+          Effect   = "Allow",
+          Resource = "*",
+        }
+      ]
+    })
+  }
+
   assume_role_policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
+    Version = "2012-10-17",
+    Statement = [
       {
-        "Effect" : "Allow",
-        "Principal" : {
-          "Service" : "ec2.amazonaws.com"
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com",
         },
-        "Action" : "sts:AssumeRole"
-      },
-      {
-        "Effect" : "Allow",
-        "Principal" : {
-          "Service" : "lambda.amazonaws.com"
-        },
-        "Action" : "sts:AssumeRole"
-      },
-      {
-        "Effect" : "Allow",
-        "Principal" : {
-          "Service" : "sqs.amazonaws.com"
-        },
-        "Action" : "sts:AssumeRole"
+        Action = "sts:AssumeRole",
       },
     ]
   })
@@ -32,22 +36,40 @@ resource "aws_iam_role" "worker_lambda_role" {
 resource "aws_iam_role" "server_lambda_role" {
   name = "APIServer"
 
+  inline_policy {
+    name = "allow_sqs_queue_access"
+
+    policy = jsonencode({
+      Version = "2012-10-17",
+      Statement = [
+        {
+          Action = [
+            "sqs:SendMessage",
+            "sqs:ReceiveMessage",
+          ],
+          Effect   = "Allow",
+          Resource = "*",
+        }
+      ]
+    })
+  }
+
   assume_role_policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
+    Version = "2012-10-17",
+    Statement = [
       {
-        "Effect" : "Allow",
-        "Principal" : {
-          "Service" : "ec2.amazonaws.com"
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com",
         },
-        "Action" : "sts:AssumeRole"
+        Action = "sts:AssumeRole",
       },
       {
-        "Effect" : "Allow",
-        "Principal" : {
-          "Service" : "lambda.amazonaws.com"
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com",
         },
-        "Action" : "sts:AssumeRole"
+        Action = "sts:AssumeRole",
       }
     ]
   })
