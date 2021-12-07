b281e055d0abf7611feeb85f39ce55fac5ee4db5
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2021-12-07 01:50:33 -0600

    re-add the queue and try to fix broken schedule expression

--- environments/dev/terraform/worker_chores.tf
+++ environments/dev/terraform/worker_chores.tf
@@ -1,3 +1,14 @@
+resource "aws_sqs_queue" "chores_queue" {
+  name       = "chores.fifo"
+  fifo_queue = true
+}
+
+resource "aws_ssm_parameter" "chores_queue_parameter" {
+  name  = "PRIXFIXE_CHORES_QUEUE_URL"
+  type  = "String"
+  value = aws_sqs_queue.chores_queue.url
+}
+
 data "archive_file" "chores_dummy" {
   type        = "zip"
   output_path = "${path.module}/archives_lambda.zip"
@@ -23,10 +34,15 @@ resource "aws_lambda_function" "chores_worker_lambda" {
   filename = data.archive_file.chores_dummy.output_path
 }
 
+resource "aws_lambda_event_source_mapping" "chores_mapping" {
+  event_source_arn = aws_sqs_queue.chores_queue.arn
+  function_name    = aws_lambda_function.chores_worker_lambda.arn
+}
+
 resource "aws_cloudwatch_event_rule" "every_minute" {
   name                = "every-minute"
-  description         = "Fires every five minutes"
-  schedule_expression = "rate(1 minutes)"
+  description         = "Fires every minute"
+  schedule_expression = "rate(1 minute)"
 }
 
 resource "aws_cloudwatch_event_target" "run_chores_every_minute" {
