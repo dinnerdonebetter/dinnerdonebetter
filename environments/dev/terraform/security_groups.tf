d3904ce92a590d4aefd45b4ef1de79e729a9dc75
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2021-12-08 17:12:41 -0600

    spin 'er up

--- environments/dev/terraform/security_groups.tf
+++ environments/dev/terraform/security_groups.tf
@@ -15,6 +15,14 @@ resource "aws_security_group" "allow_postgres" {
     ]
   }
 
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   tags = {
     Name = "allow_intra_vpc_postgres"
   }
