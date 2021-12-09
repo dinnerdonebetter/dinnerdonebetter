ac3f20bb04578d759348135f76de490fa1b06ce3
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2021-12-09 01:58:29 -0600

    turn search on

--- environments/dev/terraform/search.tf
+++ environments/dev/terraform/search.tf
@@ -1,59 +1,62 @@
-# resource "aws_cloudwatch_log_resource_policy" "dev" {
-#   policy_name = "dev"
+resource "aws_cloudwatch_log_resource_policy" "dev" {
+  policy_name = "dev"
 
-#   policy_document = <<CONFIG
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [
-#     {
-#       "Effect": "Allow",
-#       "Principal": {
-#         "Service": "es.amazonaws.com"
-#       },
-#       "Action": [
-#         "logs:PutLogEvents",
-#         "logs:PutLogEventsBatch",
-#         "logs:CreateLogStream"
-#       ],
-#       "Resource": "arn:aws:logs:*"
-#     }
-#   ]
-# }
-# CONFIG
-# }
+  policy_document = <<CONFIG
+ {
+   "Version": "2012-10-17",
+   "Statement": [
+     {
+       "Effect": "Allow",
+       "Principal": {
+         "Service": "es.amazonaws.com"
+       },
+       "Action": [
+         "logs:PutLogEvents",
+         "logs:PutLogEventsBatch",
+         "logs:CreateLogStream"
+       ],
+       "Resource": "arn:aws:logs:*"
+     }
+   ]
+ }
+ CONFIG
+}
 
-# resource "aws_elasticsearch_domain" "search" {
-#   domain_name           = "dev-search"
-#   elasticsearch_version = "2.3"
+resource "aws_iam_service_linked_role" "es" {
+  aws_service_name = "es.amazonaws.com"
+}
 
-#   cluster_config {
-#     instance_type = "t2.micro.elasticsearch"
-#   }
+resource "aws_elasticsearch_domain" "search" {
+  domain_name           = "dev-search"
+  elasticsearch_version = "2.3"
 
-#   ebs_options {
-#     ebs_enabled = true
-#     volume_size = 10
-#   }
+  cluster_config {
+    instance_type = "t2.micro.elasticsearch"
+  }
 
-#   log_publishing_options {
-#     cloudwatch_log_group_arn = aws_cloudwatch_log_group.dev.arn
-#     log_type                 = "INDEX_SLOW_LOGS"
-#   }
+  ebs_options {
+    ebs_enabled = true
+    volume_size = 10
+  }
 
-#   vpc_options {
-#     subnet_ids = [aws_subnet.private_subnets["us-east-1a"].id]
+  log_publishing_options {
+    cloudwatch_log_group_arn = aws_cloudwatch_log_group.dev.arn
+    log_type                 = "INDEX_SLOW_LOGS"
+  }
 
-#     security_group_ids = [
-#       aws_security_group.search.id,
-#     ]
-#   }
+  vpc_options {
+    subnet_ids = [aws_subnet.private_subnets["us-east-1a"].id]
 
-#   depends_on = [aws_iam_service_linked_role.es]
-# }
+    security_group_ids = [
+      aws_security_group.search.id,
+    ]
+  }
+
+  depends_on = [aws_iam_service_linked_role.es]
+}
 
 resource "aws_ssm_parameter" "search_url" {
-  name = "PRIXFIXE_ELASTICSEARCH_INSTANCE_URL"
-  type = "String"
-  # value = aws_elasticsearch_domain.search.arn
-  value = "https://localhost:9200"
+  name  = "PRIXFIXE_ELASTICSEARCH_INSTANCE_URL"
+  type  = "String"
+  value = aws_elasticsearch_domain.search.arn
 }
