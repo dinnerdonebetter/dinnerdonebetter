f6d8542336c027827fd5a7ddb1b6a9c3201edd84
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2021-12-08 19:05:47 -0600

    idefk

--- environments/dev/terraform/security_groups.tf
+++ environments/dev/terraform/security_groups.tf
@@ -16,9 +16,9 @@ resource "aws_security_group" "allow_postgres" {
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
+    from_port        = 5432
+    to_port          = 5432
+    protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
