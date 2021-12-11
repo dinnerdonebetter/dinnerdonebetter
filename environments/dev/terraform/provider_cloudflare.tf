20827edfb18b67dc67f91435156569d4f671b953
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2021-12-10 18:57:12 -0600

    whoops

--- environments/dev/terraform/provider_cloudflare.tf
+++ environments/dev/terraform/provider_cloudflare.tf
@@ -1,7 +1,7 @@
-variable "CLOUDFLARE_API_KEY" {}
+variable "CLOUDFLARE_API_TOKEN" {}
 variable "CLOUDFLARE_ZONE_ID" {}
 
 
 provider "cloudflare" {
-  api_token = var.CLOUDFLARE_API_KEY
+  api_token = var.CLOUDFLARE_API_TOKEN
 }
