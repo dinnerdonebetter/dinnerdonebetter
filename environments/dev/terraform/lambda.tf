7a26d04c6ae52944de5cafeb9b7dc7ef7fd2adb0
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2021-12-12 02:37:26 -0600

    try to exclude lambda logs

--- environments/dev/terraform/lambda.tf
+++ environments/dev/terraform/lambda.tf
@@ -3,4 +3,6 @@ locals {
   lambda_handler = "main"
   memory_size    = 128
   timeout        = 15
+
+  cloudwatch_exclude_lambda_events_filter = "[timestamp != \"START\" && timestamp != \"END \" &&timestamp != \"REPORT\" && timestamp != \"RequestId: \", aws_request_id, log]"
 }
