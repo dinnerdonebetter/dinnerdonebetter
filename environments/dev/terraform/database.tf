c1f35dbd4486887654c7f0b9166cc231e4b9dbe8
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2021-12-21 20:18:45 -0600

    terraformat

--- environments/dev/terraform/database.tf
+++ environments/dev/terraform/database.tf
@@ -106,10 +106,10 @@ resource "aws_security_group" "database" {
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
 }
