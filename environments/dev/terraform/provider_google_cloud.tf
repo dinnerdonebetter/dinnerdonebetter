40f6f67adc72bc0d8cbcc029bfcf33ab2daabae9
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2022-01-25 21:10:17 -0600

    try again

--- environments/dev/terraform/provider_google_cloud.tf
+++ environments/dev/terraform/provider_google_cloud.tf
@@ -1,7 +1,8 @@
-variable "GOOGLE_CREDENTIALS" {}
+variable "GCP_CREDENTIALS_DEV" {}
 
 provider "google" {
   project = "prixfixe-dev"
   region  = "us-central1"
   zone    = "us-central1-c"
+  credentials = var.GCP_CREDENTIALS_DEV
 }
