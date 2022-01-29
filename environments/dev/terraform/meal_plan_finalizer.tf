935eab9fe04eed4661341dad8b0fd05b662bfcd9
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2022-01-29 09:38:51 -0600

    try to separate the finalizer again

--- environments/dev/terraform/meal_plan_finalizer.tf
+++ environments/dev/terraform/meal_plan_finalizer.tf
@@ -7,8 +7,144 @@ resource "aws_ecr_repository" "meal_plan_finalizer" {
   }
 }
 
+resource "aws_security_group" "meal_plan_finalizer" {
+  name        = "meal_plan_finalizer"
+  description = "meal plan finalizer service"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "TCP"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "TCP"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
 resource "aws_cloudwatch_log_group" "meal_plan_finalizer" {
   name              = "/ecs/meal_plan_finalizer"
   retention_in_days = local.log_retention_period_in_days
 }
 
+resource "aws_cloudwatch_log_group" "meal_plan_finalizer_sidecar" {
+  name              = "/ecs/dev_meal_plan_finalizer_telemetry_collector_sidecar"
+  retention_in_days = local.log_retention_period_in_days
+}
+
+resource "aws_iam_role" "meal_plan_finalizer_task_execution_role" {
+  name               = "meal_plan_finalizer_task_execution_role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_assume_role.json
+}
+
+resource "aws_iam_role" "meal_plan_finalizer_task_role" {
+  name = "meal_plan_finalizer_task_role"
+
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role.json
+
+  inline_policy {
+    name   = "allow_sqs_queue_access"
+    policy = data.aws_iam_policy_document.allow_to_manipulate_queues.json
+  }
+
+  inline_policy {
+    name   = "allow_ssm_access"
+    policy = data.aws_iam_policy_document.allow_parameter_store_access.json
+  }
+
+  inline_policy {
+    name   = "allow_decrypt_ssm_parameters"
+    policy = data.aws_iam_policy_document.allow_to_decrypt_parameters.json
+  }
+}
+
+resource "aws_ecs_task_definition" "meal_plan_finalizer_task" {
+  family = "dev_meal_plan_finalizer"
+
+  container_definitions = jsonencode([
+    {
+      name : "otel-collector",
+      image : format("%s:latest", aws_ecr_repository.otel_collector.repository_url)
+      essential : true,
+      logConfiguration : {
+        logDriver : "awslogs",
+        options : {
+          awslogs-region : local.aws_region,
+          awslogs-group : aws_cloudwatch_log_group.meal_plan_finalizer_sidecar.name,
+          awslogs-create-group : "true",
+          awslogs-stream-prefix : "otel-collector"
+        }
+      }
+    },
+    {
+      name  = "meal_plan_finalizer",
+      image = format("%s:latest", aws_ecr_repository.meal_plan_finalizer.repository_url),
+      essential : true,
+      logConfiguration : {
+        logDriver : "awslogs",
+        options : {
+          awslogs-region : local.aws_region,
+          awslogs-group : aws_cloudwatch_log_group.meal_plan_finalizer.name,
+          awslogs-stream-prefix : "ecs",
+        },
+      },
+    },
+  ])
+
+  execution_role_arn = aws_iam_role.meal_plan_finalizer_task_execution_role.arn
+  task_role_arn      = aws_iam_role.meal_plan_finalizer_task_role.arn
+
+  # These are the minimum values for Fargate containers.
+  cpu                      = 256
+  memory                   = 512
+  requires_compatibilities = ["FARGATE"]
+
+  network_mode = "awsvpc"
+}
+
+resource "aws_ecs_service" "meal_plan_finalizer" {
+  name                               = "meal_plan_finalizer"
+  task_definition                    = aws_ecs_task_definition.meal_plan_finalizer_task.arn
+  cluster                            = aws_ecs_cluster.dev.id
+  launch_type                        = "FARGATE"
+  deployment_maximum_percent         = 200
+  deployment_minimum_healthy_percent = 100
+  desired_count                      = 1
+
+  deployment_controller {
+    type = "ECS"
+  }
+
+  deployment_circuit_breaker {
+    enable   = true
+    rollback = true
+  }
+
+  network_configuration {
+    assign_public_ip = true
+
+    security_groups = [
+      aws_security_group.meal_plan_finalizer.id,
+    ]
+
+    subnets = concat(
+    [for x in aws_subnet.public_subnets : x.id],
+    [for x in aws_subnet.private_subnets : x.id],
+    )
+  }
+}
