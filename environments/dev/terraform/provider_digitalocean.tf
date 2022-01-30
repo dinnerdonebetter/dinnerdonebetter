3b58683c852a308600a659632b69c1abe2baf820
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2022-01-29 21:10:50 -0600

    whew again

--- environments/dev/terraform/provider_digitalocean.tf
+++ environments/dev/terraform/provider_digitalocean.tf
@@ -1,10 +1,13 @@
 variable "DIGITAL_OCEAN_API_TOKEN" {}
-#variable "SPACES_ACCESS_KEY_ID" {}
-#variable "SPACES_SECRET_ACCESS_KEY" {}
+variable "SPACES_ACCESS_KEY_ID" {}
+variable "SPACES_SECRET_ACCESS_KEY" {}
 
 # Configure the DigitalOcean Provider
 provider "digitalocean" {
   token = var.DIGITAL_OCEAN_API_TOKEN
+
+  spaces_access_id  = var.SPACES_ACCESS_KEY_ID
+  spaces_secret_key = var.SPACES_SECRET_ACCESS_KEY
 }
 
 resource "digitalocean_container_registry" "dev" {
