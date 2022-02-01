e91fd6554288bcde093b58c2007edf834a0df286
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2022-02-01 02:38:35 -0600

    try again #14

--- environments/dev/terraform/network.tf
+++ environments/dev/terraform/network.tf
@@ -3,3 +3,10 @@ resource "digitalocean_vpc" "dev" {
   region   = local.region
   ip_range = "10.10.10.0/24"
 }
+
+resource "digitalocean_project_resources" "vpc" {
+  project = digitalocean_project.prixfixe_dev.id
+  resources = [
+    digitalocean_vpc.dev.urn
+  ]
+}
