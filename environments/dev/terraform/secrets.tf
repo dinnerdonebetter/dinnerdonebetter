1d7c1e0b2e23ad049dcba9fbabb581786345204a
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2022-02-03 23:44:28 -0600

    it's a brand new day #18 (try to actually connect to the database)

--- environments/dev/terraform/secrets.tf
+++ environments/dev/terraform/secrets.tf
@@ -1,3 +1,6 @@
+variable "SEGMENT_API_TOKEN" {}
+variable "SENDGRID_API_TOKEN" {}
+
 resource "random_string" "cookie_hash_key" {
   length  = 64
   special = false
@@ -23,5 +26,27 @@ resource "kubernetes_secret_v1" "config_auth" {
     "cookie_hash_key"  = random_string.cookie_hash_key.result
     "cookie_block_key" = random_string.cookie_block_key.result
     "paseto_local_key" = random_string.paseto_local_key.result
+  }
+}
+
+resource "kubernetes_secret_v1" "config_sendgrid" {
+  metadata {
+    namespace = local.kubernetes_namespace
+    name      = "config.third-party.sendgrid"
+  }
+
+  data = {
+    "sendgrid_api_token" = var.SENDGRID_API_TOKEN
+  }
+}
+
+resource "kubernetes_secret_v1" "config_segment" {
+  metadata {
+    namespace = local.kubernetes_namespace
+    name      = "config.third-party.segment"
+  }
+
+  data = {
+    "segment_api_token" = var.SEGMENT_API_TOKEN
   }
 }
