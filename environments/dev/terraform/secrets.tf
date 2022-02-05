ad1374eaedc837f8e42b23c5af1a58b9d8a56984
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2022-02-05 12:03:30 -0600

    live from my office, it's saturday kubernetes bullshit! #4

--- environments/dev/terraform/secrets.tf
+++ environments/dev/terraform/secrets.tf
@@ -50,3 +50,14 @@ resource "kubernetes_secret_v1" "config_segment" {
     "segment_api_token" = var.SEGMENT_API_TOKEN
   }
 }
+
+resource "kubernetes_secret_v1" "api_config" {
+  metadata {
+    namespace = local.kubernetes_namespace
+    name      = "prixfixe-api-configuration"
+  }
+
+  data = {
+    "service-config.json" = file("${path.module}/service-config.json")
+  }
+}
