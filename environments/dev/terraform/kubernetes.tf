69a8c767dd2d7d29ecd3fa3c96eae327f93e2fc7
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2022-02-04 20:22:09 -0600

    day 900, it feels like #6

--- environments/dev/terraform/kubernetes.tf
+++ environments/dev/terraform/kubernetes.tf
@@ -54,7 +54,7 @@ provider "helm" {
 }
 
 resource "helm_release" "external_dns" {
-  name       = "external_dns"
+  name       = "external-dns"
   repository = "https://charts.bitnami.com/bitnami"
   chart      = "external-dns"
 
