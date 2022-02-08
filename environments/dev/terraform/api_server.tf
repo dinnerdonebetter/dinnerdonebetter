131ab477a98963483c13cc7079f1f5d48fd08b14
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2022-02-07 21:06:43 -0600

    trying to get GCP to work again #6

--- environments/dev/terraform/api_server.tf
+++ environments/dev/terraform/api_server.tf
@@ -2,16 +2,16 @@ locals {
   public_url = "api.prixfixe.dev"
 }
 
+resource "google_service_account" "api_server_account" {
+  account_id   = "api-server"
+  display_name = "API Server"
+}
+
 resource "google_project_iam_member" "project" {
   project = local.project_id
   role    = "roles/editor"
-  member  = "user:api-server@prixfixe.dev"
+  member  = format("serviceAccount:%s", google_service_account.api_server_account.email)
 }
-
-#resource "google_service_account" "api_server_account" {
-#  account_id   = "api-server"
-#  display_name = "API Server"
-#}
 
 resource "google_cloud_run_service" "api_server" {
   name     = "api-server"
@@ -26,7 +26,7 @@ resource "google_cloud_run_service" "api_server" {
 
   template {
     spec {
-      #      service_account_name = google_service_account.api_server_account.email
+      # service_account_name = google_service_account.api_server_account.email
 
       containers {
         image = "gcr.io/prixfixe-dev/api_server"
