eac6b44faaf1f57581797717f1a29c3528208f38
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2022-02-08 21:52:23 -0600

    trying to get data changes thing to work #13

--- environments/dev/terraform/data_changes.tf
+++ environments/dev/terraform/data_changes.tf
@@ -2,15 +2,25 @@ resource "google_pubsub_topic" "data_changes_topic" {
   name = "data_changes"
 }
 
-resource "google_storage_bucket" "data_changes_cloud_function_bucket" {
-  name     = "data-changes-cloud-function"
+data "archive_file" "dummy_zip" {
+  type        = "zip"
+  output_path = "${path.module}/data_changes_function.zip"
+
+  source {
+    content  = "hello"
+    filename = "dummy.txt"
+  }
+}
+
+resource "google_storage_bucket" "bucket" {
+  name     = "data-changes-function"
   location = "US"
 }
 
 resource "google_storage_bucket_object" "archive" {
   name   = "data_changes_function.zip"
-  bucket = google_storage_bucket.data_changes_cloud_function_bucket.name
-  source = "${path.module}/data_changes_function.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = data.archive_file.dummy_zip.output_path
 }
 
 resource "google_cloudfunctions_function" "data_changes" {
@@ -19,7 +29,7 @@ resource "google_cloudfunctions_function" "data_changes" {
   runtime     = local.go_runtime
 
   available_memory_mb   = 128
-  source_archive_bucket = google_storage_bucket.data_changes_cloud_function_bucket.name
+  source_archive_bucket = google_storage_bucket.bucket.name
   source_archive_object = google_storage_bucket_object.archive.name
 
   event_trigger {
