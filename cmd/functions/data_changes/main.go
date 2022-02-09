6cc5c485433326d8667031945e5bef6d3a0806a8
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2022-02-08 19:56:57 -0600

    trying to get data changes thing to work #4

--- cmd/functions/data_changes/main.go
+++ cmd/functions/data_changes/main.go
@@ -2,68 +2,18 @@ package main
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
 	"log"
-	"net/http"
-	"os"
-
-	"cloud.google.com/go/pubsub"
 )
 
-// GOOGLE_CLOUD_PROJECT is a user-set environment variable.
-var projectID = os.Getenv("GOOGLE_CLOUD_PROJECT")
-
-// client is a global Pub/Sub client, initialized once per instance.
-var client *pubsub.Client
-
-func init() {
-	// err is pre-declared to avoid shadowing client.
-	var err error
-
-	// client is initialized with context.Background() because it should
-	// persist between function invocations.
-	client, err = pubsub.NewClient(context.Background(), projectID)
-	if err != nil {
-		log.Fatalf("pubsub.NewClient: %v", err)
-	}
+// PubSubMessage is the payload of a Pub/Sub event. See the documentation for more details:
+// https://cloud.google.com/pubsub/docs/reference/rest/v1/PubsubMessage
+type PubSubMessage struct {
+	Data []byte `json:"data"`
 }
 
-type publishRequest struct {
-	Topic   string `json:"topic"`
-	Message string `json:"message"`
-}
+// ProcessDataChange handles a data change
+func ProcessDataChange(ctx context.Context, m PubSubMessage) error {
+	log.Println("invoked")
 
-// PublishMessage publishes a message to Pub/Sub. PublishMessage only works
-// with topics that already exist.
-func PublishMessage(w http.ResponseWriter, r *http.Request) {
-	// Parse the request body to get the topic name and message.
-	p := publishRequest{}
-
-	if err := json.NewDecoder(r.Body).Decode(&p); err != nil {
-		log.Printf("json.NewDecoder: %v", err)
-		http.Error(w, "Error parsing request", http.StatusBadRequest)
-		return
-	}
-
-	if p.Topic == "" || p.Message == "" {
-		s := "missing 'topic' or 'message' parameter"
-		log.Println(s)
-		http.Error(w, s, http.StatusBadRequest)
-		return
-	}
-
-	m := &pubsub.Message{
-		Data: []byte(p.Message),
-	}
-	// Publish and Get use r.Context() because they are only needed for this
-	// function invocation. If this were a background function, they would use
-	// the ctx passed as an argument.
-	id, err := client.Topic(p.Topic).Publish(r.Context(), m).Get(r.Context())
-	if err != nil {
-		log.Printf("topic(%s).Publish.Get: %v", p.Topic, err)
-		http.Error(w, "Error publishing message", http.StatusInternalServerError)
-		return
-	}
-	fmt.Fprintf(w, "Message published: %v", id)
+	return nil
 }
