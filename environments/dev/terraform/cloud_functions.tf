483fd1b4373d054fa8ba06c8815e16a0b78a480d
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2022-02-08 19:39:02 -0600

    trying to get data changes thing to work #2

--- environments/dev/terraform/cloud_functions.tf
+++ environments/dev/terraform/cloud_functions.tf
@@ -1,3 +1,4 @@
 locals {
-  go_runtime = "go116"
+  go_runtime                 = "go116"
+  pubsub_topic_publish_event = "google.pubsub.topic.publish"
 }
--- environments/dev/terraform/data_changes.tf
+++ environments/dev/terraform/data_changes.tf
@@ -33,7 +33,7 @@ resource "google_cloudfunctions_function" "data_changes" {
   source_archive_object = google_storage_bucket_object.archive.name
 
   event_trigger {
-    event_type = "google.pubsub.topic.publish"
+    event_type = local.pubsub_topic_publish_event
     resource   = google_pubsub_topic.data_changes_topic.name
   }
 
