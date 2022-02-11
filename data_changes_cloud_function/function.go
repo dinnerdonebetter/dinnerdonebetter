f79d4e84174b672c95eeebfe8f64b21ba1214d2b
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2022-02-11 17:28:27 -0600

    try to get cloud functions deploying properly (#3)

---
+++ data_changes_cloud_function/function.go
@@ -0,0 +1,24 @@
+package datachangesfunction
+
+import (
+	"context"
+
+	_ "github.com/GoogleCloudPlatform/functions-framework-go/funcframework"
+
+	"github.com/prixfixeco/api_server/internal/observability/logging/zerolog"
+)
+
+// PubSubMessage is the payload of a Pub/Sub event. See the documentation for more details:
+// https://cloud.google.com/pubsub/docs/reference/rest/v1/PubsubMessage
+type PubSubMessage struct {
+	Data []byte `json:"data"`
+}
+
+// ProcessDataChange handles a data change.
+func ProcessDataChange(ctx context.Context, m PubSubMessage) error {
+	logger := zerolog.NewZerologLogger()
+
+	logger.Info("invoked")
+
+	return nil
+}
