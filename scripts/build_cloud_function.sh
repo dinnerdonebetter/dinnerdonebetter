207f6180658eb8ed73aa38a05b2a0ea93f8d14f5
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2022-02-10 20:54:12 -0600

    try to get working meal plan finalizer things (#2)

--- scripts/build_cloud_function.sh
+++ scripts/build_cloud_function.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 mkdir -p $1
-cp cmd/functions/data_changes/function.go $1/function.go
+cp cmd/functions/$1/function.go $1/function.go
 sed "s/replace\sgithub\.com\/prixfixeco\/api_server\s=>\s\.\.\/\.\.\/\.\.\//replace github\.com\/prixfixeco\/api_server => \.\.\//" cmd/functions/$1/go.mod > $1/go.mod
 (cd $1 && go mod tidy && go mod vendor)
 (cd $1 && zip -r -D $1_cloud_function.zip * && mv $1_cloud_function.zip ../)
