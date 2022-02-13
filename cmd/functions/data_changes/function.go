76ada2b5f211519ad1db517807ef8c0f92bb1d4d
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2022-02-13 15:46:16 -0600

    try to get the data changes queue to work (#14)

--- cmd/functions/data_changes/function.go
+++ cmd/functions/data_changes/function.go
@@ -2,15 +2,16 @@ package datachangesfunction
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+
+	"github.com/prixfixeco/api_server/pkg/types"
 
 	"github.com/prixfixeco/api_server/internal/observability"
 
 	"github.com/prixfixeco/api_server/internal/config"
 	customerdataconfig "github.com/prixfixeco/api_server/internal/customerdata/config"
 	"github.com/prixfixeco/api_server/internal/observability/keys"
-
-	"github.com/prixfixeco/api_server/pkg/types"
 
 	_ "github.com/GoogleCloudPlatform/functions-framework-go/funcframework"
 
@@ -20,7 +21,7 @@ import (
 // PubSubMessage is the payload of a Pub/Sub event. See the documentation for more details:
 // https://cloud.google.com/pubsub/docs/reference/rest/v1/PubsubMessage
 type PubSubMessage struct {
-	Data types.DataChangeMessage `json:"data"`
+	Data string `json:"data"`
 }
 
 // ProcessDataChange handles a data change.
@@ -37,18 +38,23 @@ func ProcessDataChange(ctx context.Context, m PubSubMessage) error {
 		return fmt.Errorf("error setting up customer data collector: %w", err)
 	}
 
-	switch m.Data.MessageType {
+	var changeMessage types.DataChangeMessage
+	if unmarshalErr := json.Unmarshal([]byte(m.Data), &changeMessage); unmarshalErr != nil {
+		observability.AcknowledgeError(unmarshalErr, logger, nil, "unmarshalling data change message")
+	}
+
+	switch changeMessage.MessageType {
 	case "meal_plan_finalized":
 		// alert family members or whatever
 		if err = customerDataCollector.EventOccurred(ctx, "meal_plan_finalized", "meal-plan-finalizer", map[string]interface{}{
-			keys.HouseholdIDKey: m.Data.MealPlan.BelongsToHousehold,
-			keys.MealPlanIDKey:  m.Data.MealPlan.ID,
+			keys.HouseholdIDKey: changeMessage.MealPlan.BelongsToHousehold,
+			keys.MealPlanIDKey:  changeMessage.MealPlan.ID,
 		}); err != nil {
 			observability.AcknowledgeError(err, logger, nil, "notifying customer data platform")
 		}
 	}
 
-	logger.WithValue("payload", m.Data).Info("invoked")
+	logger.WithValue("payload", changeMessage).Info("invoked")
 
 	return nil
 }
