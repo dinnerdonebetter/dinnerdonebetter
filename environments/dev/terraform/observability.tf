a289c10a3d207d0aee671a7fb93b55690164be45
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2022-02-13 22:06:42 -0600

    try to get the data changes queue to work as intended (#23)

---
+++ environments/dev/terraform/observability.tf
@@ -0,0 +1,19 @@
+resource "google_monitoring_uptime_check_config" "api_uptime" {
+  display_name = "api-server-uptime-check"
+  timeout      = "60s"
+
+  http_check {
+    path         = "/_meta_/health"
+    port         = "443"
+    use_ssl      = true
+    validate_ssl = true
+  }
+
+  monitored_resource {
+    type = "uptime_url"
+    labels = {
+      project_id = local.project_id
+      host       = "api.prixfixe.dev"
+    }
+  }
+}
