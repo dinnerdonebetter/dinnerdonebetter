931d34346b558e3a2794c9b32ca1015d08bd408f
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2022-02-14 23:59:14 -0600

    done, maybe

--- environments/dev/terraform/_terraform_.tf
+++ environments/dev/terraform/_terraform_.tf
@@ -15,7 +15,7 @@ terraform {
     }
     google = {
       source  = "hashicorp/google"
-      version = "4.8.0"
+      version = "4.10.0"
     }
     archive = {
       source  = "hashicorp/archive"
