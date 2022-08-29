ab4b7ca1c51cfec8be2e84ff35369a8979173dd8
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2022-08-28 22:45:57 -0500

    add plan

--- internal/database/postgres/migrations/00038_advanced_prep_notifications.sql
+++ internal/database/postgres/migrations/00038_advanced_prep_notifications.sql
@@ -18,3 +18,4 @@ CREATE TABLE IF NOT EXISTS advanced_prep_notifications (
 ALTER TABLE valid_instruments ADD COLUMN "usable_for_storage" BOOLEAN NOT NULL DEFAULT 'false';
 ALTER TABLE valid_ingredients ADD COLUMN "storage_instructions" TEXT NOT NULL DEFAULT '';
 ALTER TABLE recipe_step_products ADD COLUMN "storage_instructions" TEXT NOT NULL DEFAULT '';
+ALTER TABLE meal_plan_options ADD COLUMN "prep_steps_created" BOOLEAN NOT NULL DEFAULT '';
