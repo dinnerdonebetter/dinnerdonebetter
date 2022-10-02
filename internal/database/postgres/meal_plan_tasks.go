bda0d3f3063669d7505813d743c343d75d108c79
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2022-10-02 15:53:38 -0500

    get some more testing going (but not enough)

--- internal/database/postgres/meal_plan_tasks.go
+++ internal/database/postgres/meal_plan_tasks.go
@@ -350,7 +350,19 @@ func (q *Querier) GetMealPlanTasksForMealPlan(ctx context.Context, mealPlanID st
 		return nil, observability.PrepareAndLogError(scanErr, logger, span, "scanning meal plan tasks")
 	}
 
-	// TODO: saturate recipe steps here
+	for i, mealPlanTask := range x {
+		recipeSteps := []*types.RecipeStep{}
+		for _, step := range mealPlanTask.RecipeSteps {
+			recipeStep, recipeStepGetErr := q.getRecipeStepByID(ctx, q.db, step.ID)
+			if recipeStepGetErr != nil {
+				return nil, observability.PrepareAndLogError(recipeStepGetErr, logger, span, "getting recipe step for meal plan tasks")
+			}
+
+			recipeSteps = append(recipeSteps, recipeStep)
+		}
+
+		x[i].RecipeSteps = recipeSteps
+	}
 
 	logger.Info("meal plan tasks retrieved")
 
