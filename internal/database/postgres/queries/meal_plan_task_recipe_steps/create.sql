291fbb6d05578610b3f7df24adf6af371d10dfa4
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2022-10-04 21:59:46 -0500

    fixed

--- internal/database/postgres/queries/meal_plan_task_recipe_steps/create.sql
+++ internal/database/postgres/queries/meal_plan_task_recipe_steps/create.sql
@@ -1,2 +1,2 @@
-INSERT INTO meal_plan_task_recipe_steps (id,belongs_to_meal_plan_task,applies_to_recipe_step,satisfies_recipe_step)
+INSERT INTO meal_plan_task_recipe_steps (id,belongs_to_meal_plan_task,attributable_to_recipe_step,satisfies_recipe_step)
 VALUES ($1,$2,$3,$4);
