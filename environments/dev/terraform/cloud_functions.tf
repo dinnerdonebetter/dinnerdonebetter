4c3ec469f08d11804f8a3d64368b3d8a21f0738f
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2022-11-09 19:07:37 -0600

    attempt deploy fix pt. 4

--- environments/dev/terraform/cloud_functions.tf
+++ environments/dev/terraform/cloud_functions.tf
@@ -1,5 +1,5 @@
 locals {
   go_runtime                 = "go119"
-  available_memory           = 128
+  available_memory           = "128Mi"
   pubsub_topic_publish_event = "google.cloud.pubsub.topic.v1.messagePublished"
 }
