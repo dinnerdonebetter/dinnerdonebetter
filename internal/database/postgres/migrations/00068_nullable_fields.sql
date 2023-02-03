921a24255014cc11197d2587b5103ae45702a4a9
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2023-02-03 01:58:41 -0600

    farewell for now

--- internal/database/postgres/migrations/00068_nullable_fields.sql
+++ internal/database/postgres/migrations/00068_nullable_fields.sql
@@ -14,8 +14,8 @@ ALTER TABLE recipe_step_instruments ALTER COLUMN maximum_quantity DROP NOT NULL;
 ALTER TABLE recipe_step_instruments ALTER COLUMN maximum_quantity DROP DEFAULT;
 ALTER TABLE recipe_step_instruments ALTER COLUMN maximum_quantity DROP NOT NULL;
 
-ALTER TABLE recipe_step_ingredients ALTER COLUMN minimum_quantity_value DROP DEFAULT;
-ALTER TABLE recipe_step_ingredients ALTER COLUMN minimum_quantity_value DROP NOT NULL;
+ALTER TABLE recipe_step_ingredients ALTER COLUMN maximum_quantity_value DROP DEFAULT;
+ALTER TABLE recipe_step_ingredients ALTER COLUMN maximum_quantity_value DROP NOT NULL;
 
 ALTER TABLE meal_plan_grocery_list_items ALTER COLUMN maximum_quantity_needed DROP DEFAULT;
 ALTER TABLE meal_plan_grocery_list_items ALTER COLUMN maximum_quantity_needed DROP NOT NULL;
