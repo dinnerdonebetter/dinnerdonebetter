135545722c41f2e6683656c83b79cd0c46b0c8bf
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2023-05-08 21:03:51 -0500

    add quantity scale factor to recipe step products

--- internal/database/postgres/migrations/00079_last_indexed_fields.sql
+++ internal/database/postgres/migrations/00079_last_indexed_fields.sql
@@ -6,7 +6,7 @@ ALTER TABLE valid_measurement_units ADD COLUMN "last_indexed_at" TIMESTAMP WITH 
 ALTER TABLE valid_preparations ADD COLUMN "last_indexed_at" TIMESTAMP WITH TIME ZONE DEFAULT NULL;
 
 ALTER TABLE recipe_steps ADD COLUMN "time_scale_factor" NUMERIC(10, 5) NOT NULL DEFAULT 1.0;
--- ALTER TABLE recipe_step_products ADD COLUMN "quantity_scale_factor" NUMERIC(10, 5) NOT NULL DEFAULT 1.0;
+ALTER TABLE recipe_step_products ADD COLUMN "quantity_scale_factor" NUMERIC(10, 5) NOT NULL DEFAULT 1.0;
 -- ALTER TABLE recipe_step_ingredients ADD COLUMN "quantity_scale_factor" NUMERIC(10, 5) NOT NULL DEFAULT 1.0;
 -- ALTER TABLE recipe_step_ingredients DROP COLUMN IF EXISTS "requires_defrost";
 -- ALTER TABLE recipe_step_vessels ADD COLUMN "quantity_scale_factor" NUMERIC(10, 5) NOT NULL DEFAULT 1.0;
