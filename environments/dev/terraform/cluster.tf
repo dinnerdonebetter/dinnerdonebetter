2cdabf8a99535d730e4edb67c85dca34272c4cd0
Jeffrey <verygoodsoftwarenotvirus@protonmail.com>
2023-10-06 17:53:30 -0500

    update cluster

--- environments/dev/terraform/cluster.tf
+++ environments/dev/terraform/cluster.tf
@@ -30,7 +30,7 @@ resource "google_container_node_pool" "primary_nodes" {
 
   node_config {
     oauth_scopes = [
-      "googleapis.com/auth/cloud-platform",
+      "https://www.googleapis.com/auth/cloud-platform",
     ]
 
     labels = {
