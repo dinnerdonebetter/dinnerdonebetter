4e6ad71a4d9f549dd72c4fd3d1393ba52fc75851
Jeffrey <verygoodsoftwarenotvirus@protonmail.com>
2024-10-16 17:31:17 -0500

    more logs

--- frontend/packages/next-routes/login.ts
+++ frontend/packages/next-routes/login.ts
@@ -86,7 +86,7 @@ export function buildLoginRoute(config: {
   admin: boolean;
 }) {
   return async function LoginRoute(req: NextApiRequest, res: NextApiResponse) {
-    console.log(`calling login with client ID: ${config.oauth2ClientID} and secret: ${config.oauth2ClientSecret}`);
+    console.log(`calling login with url: ${config.baseURL} client ID: ${config.oauth2ClientID} and secret: ${config.oauth2ClientSecret}`);
 
     if (config.oauth2ClientID === '' || config.oauth2ClientSecret === '') {
       throw new Error('oauth2 client id and secret must be provided');
