3bc5e3a26dfd598af38773fdb2cce060c76ab5d5
Jeffrey <verygoodsoftwarenotvirus@protonmail.com>
2024-10-16 05:32:18 -0500

    fix descriptions

--- backend/environments/dev/terraform/containers.tf
+++ backend/environments/dev/terraform/containers.tf
@@ -6,43 +6,73 @@ resource "google_artifact_registry_repository" "dev_repository" {
 }
 
 resource "google_artifact_registry_repository" "db-cleaner-container" {
-  location      = "us-central1"
+  location      = local.gcp_region
   repository_id = "db-cleaner"
   description   = "database cleaner worker image"
   format        = "DOCKER"
 }
 
 resource "google_artifact_registry_repository" "search-index-scheduler-container" {
-  location      = "us-central1"
+  location      = local.gcp_region
   repository_id = "search-index-scheduler"
-  description   = "database cleaner worker image"
+  description   = "search data index scheduler worker image"
   format        = "DOCKER"
 }
 
 resource "google_artifact_registry_repository" "email-prober-container" {
-  location      = "us-central1"
+  location      = local.gcp_region
   repository_id = "email-prober"
-  description   = "database cleaner worker image"
+  description   = "email prober worker image"
   format        = "DOCKER"
 }
 
 resource "google_artifact_registry_repository" "meal-plan-finalizer-container" {
-  location      = "us-central1"
+  location      = local.gcp_region
   repository_id = "meal-plan-finalizer"
-  description   = "database cleaner worker image"
+  description   = "meal plan finalizer worker image"
   format        = "DOCKER"
 }
 
 resource "google_artifact_registry_repository" "meal-plan-grocery-list-initializer-container" {
-  location      = "us-central1"
+  location      = local.gcp_region
   repository_id = "meal-plan-grocery-list-initializer"
-  description   = "database cleaner worker image"
+  description   = "mela plan grocery list initializer worker image"
   format        = "DOCKER"
 }
 
 resource "google_artifact_registry_repository" "meal-plan-task-creator-container" {
-  location      = "us-central1"
+  location      = local.gcp_region
   repository_id = "meal-plan-task-creator"
-  description   = "database cleaner worker image"
+  description   = "meal plan task creator worker image"
+  format        = "DOCKER"
+}
+
+resource "google_artifact_registry_repository" "data-changes-handler-container" {
+  location      = local.gcp_region
+  repository_id = "data-changes-handler"
+  description   = "data changes handler function image"
+  format        = "DOCKER"
+}
+
+resource "google_artifact_registry_repository" "email-handler-container" {
+  location      = local.gcp_region
+  repository_id = "email-handler"
+  description   = "email handler function image"
+  format        = "DOCKER"
+}
+
+
+resource "google_artifact_registry_repository" "search-indexer-container" {
+  location      = local.gcp_region
+  repository_id = "search-indexer"
+  description   = "search indexing function image"
+  format        = "DOCKER"
+}
+
+
+resource "google_artifact_registry_repository" "webhook-executor-container" {
+  location      = local.gcp_region
+  repository_id = "webhook-executor"
+  description   = "webhook execution function image"
   format        = "DOCKER"
 }
