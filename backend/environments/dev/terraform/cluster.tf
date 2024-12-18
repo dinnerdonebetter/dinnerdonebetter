06df2e7e2d7a93dfbd0148e67544010c1b83b9eb
Jeffrey D. <verygoodsoftwarenotvirus@protonmail.com>
2024-12-18 17:38:30 -0600

    create a cluster

---
+++ backend/environments/dev/terraform/cluster.tf
@@ -0,0 +1,33 @@
+resource "google_service_account" "kubernetes" {
+  account_id   = "kubernetes-service-account"
+  display_name = "Kubernetes Service Account"
+}
+
+resource "google_container_cluster" "primary" {
+  name     = "dev"
+  location = local.gcp_region
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "node-pool"
+  location   = local.gcp_region
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-medium"
+
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    service_account = google_service_account.kubernetes.email
+    oauth_scopes    = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
