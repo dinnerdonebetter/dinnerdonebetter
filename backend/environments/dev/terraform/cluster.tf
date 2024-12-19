9ffebf4e92898ecb0dc2cf861014d72072dfc599
Jeffrey D. <verygoodsoftwarenotvirus@protonmail.com>
2024-12-19 15:56:11 -0600

    cluster fix attempt number 3

--- backend/environments/dev/terraform/cluster.tf
+++ backend/environments/dev/terraform/cluster.tf
@@ -39,47 +39,47 @@ resource "google_project_iam_member" "dev_cluster" {
   member  = format("serviceAccount:%s", google_service_account.dev_cluster_service_account.email)
 }
 
-# resource "google_container_cluster" "primary" {
-#   name     = local.environment
-#   location = local.gcp_region
-#
-#   # We can't create a cluster with no node pool defined, but we want to only use
-#   # separately managed node pools. So we create the smallest possible default
-#   # node pool and immediately delete it.
-#   remove_default_node_pool = true
-#   initial_node_count       = 1
-#
-#   #  network    = google_compute_network.vpc.name
-#   #  subnetwork = google_compute_subnetwork.subnet.name
-# }
-#
-# # Separately Managed Node Pool
-# resource "google_container_node_pool" "primary_nodes" {
-#   name       = google_container_cluster.primary.name
-#   location   = local.gcp_region
-#   cluster    = google_container_cluster.primary.name
-#   node_count = 1
-#
-#   node_locations = [
-#     "us-central1-a",
-#   ]
-#
-#   node_config {
-#     oauth_scopes = [
-#       "https://www.googleapis.com/auth/cloud-platform",
-#     ]
-#
-#     service_account = google_service_account.dev_cluster_service_account.email
-#
-#     labels = {
-#       env = local.project_id
-#     }
-#
-#     preemptible  = true
-#     machine_type = "e2-medium"
-#     tags         = ["gke-node", local.environment]
-#     metadata = {
-#       disable-legacy-endpoints = "true"
-#     }
-#   }
-# }
+resource "google_container_cluster" "primary" {
+  name     = local.environment
+  location = local.gcp_region
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  #  network    = google_compute_network.vpc.name
+  #  subnetwork = google_compute_subnetwork.subnet.name
+}
+
+# Separately Managed Node Pool
+resource "google_container_node_pool" "primary_nodes" {
+  name       = google_container_cluster.primary.name
+  location   = local.gcp_region
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+
+  node_locations = [
+    "us-central1-a",
+  ]
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+
+    service_account = google_service_account.dev_cluster_service_account.email
+
+    labels = {
+      env = local.project_id
+    }
+
+    preemptible  = true
+    machine_type = "e2-medium"
+    tags         = ["gke-node", local.environment]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
+}
