5db42d901f969ae3560e92b3d734dfa1e05b1f03
Jeffrey D. <verygoodsoftwarenotvirus@protonmail.com>
2024-12-22 22:21:02 -0600

    once again?

--- backend/environments/dev/terraform/database.tf
+++ backend/environments/dev/terraform/database.tf
@@ -26,6 +26,7 @@ resource "google_service_networking_connection" "private_vpc_connection" {
   network                 = google_compute_network.private_network.id
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = []
+  ## INIT: When creating this for the first time, leave it empty
   # reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
 }
 
