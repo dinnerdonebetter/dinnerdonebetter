4251cb1c0d806654af7f3672f2270e6dfaf56d24
Jeffrey D. <verygoodsoftwarenotvirus@protonmail.com>
2025-01-04 01:55:15 -0600

    again

--- backend/deploy/environments/dev/terraform/networking.tf
+++ backend/deploy/environments/dev/terraform/networking.tf
@@ -1,17 +1,11 @@
 data "google_compute_network" "private_network" {
   provider = google
-
-  name = "private-network"
+  name     = "private-network"
 }
 
-resource "google_compute_global_address" "private_ip_address" {
+data "google_compute_global_address" "private_ip_address" {
   provider = google
-
-  name          = "private-ip-address"
-  purpose       = "VPC_PEERING"
-  address_type  = "INTERNAL"
-  prefix_length = 16
-  network       = data.google_compute_network.private_network.id
+  name     = "private-ip-address"
 }
 
 resource "google_service_networking_connection" "private_vpc_connection" {
@@ -19,5 +13,5 @@ resource "google_service_networking_connection" "private_vpc_connection" {
 
   network                 = data.google_compute_network.private_network.id
   service                 = "servicenetworking.googleapis.com"
-  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+  reserved_peering_ranges = [data.google_compute_global_address.private_ip_address.name]
 }
--- frontend/deploy/environments/dev/webapp/terraform/kubernetes.tf
+++ frontend/deploy/environments/dev/webapp/terraform/kubernetes.tf
@@ -18,7 +18,7 @@ variable "API_SERVICE_OAUTH2_CLIENT_ID" {}
 variable "API_SERVICE_OAUTH2_CLIENT_SECRET" {}
 
 resource "random_bytes" "webapp_cookie_encryption_key" {
-  length  = 32
+  length = 32
 }
 
 resource "random_bytes" "cookie_encryption_iv" {
@@ -42,12 +42,12 @@ resource "kubernetes_secret" "cloudflare_api_key" {
   depends_on = [data.google_container_cluster.dev_cluster]
 
   data = {
-    COOKIE_ENCRYPTION_KEY = random_bytes.webapp_cookie_encryption_key.hex
-    BASE64_COOKIE_ENCRYPT_IV =  random_bytes.cookie_encryption_iv.base64
-    API_SERVICE_OAUTH2_CLIENT_ID = var.API_SERVICE_OAUTH2_CLIENT_ID
-    API_SERVICE_OAUTH2_CLIENT_SECRET = var.API_SERVICE_OAUTH2_CLIENT_SECRET
-    SEGMENT_API_TOKEN = var.SEGMENT_API_TOKEN
-    DINNER_DONE_BETTER_OAUTH2_CLIENT_ID = var.DINNER_DONE_BETTER_OAUTH2_CLIENT_ID
+    COOKIE_ENCRYPTION_KEY                   = random_bytes.webapp_cookie_encryption_key.hex
+    BASE64_COOKIE_ENCRYPT_IV                = random_bytes.cookie_encryption_iv.base64
+    API_SERVICE_OAUTH2_CLIENT_ID            = var.API_SERVICE_OAUTH2_CLIENT_ID
+    API_SERVICE_OAUTH2_CLIENT_SECRET        = var.API_SERVICE_OAUTH2_CLIENT_SECRET
+    SEGMENT_API_TOKEN                       = var.SEGMENT_API_TOKEN
+    DINNER_DONE_BETTER_OAUTH2_CLIENT_ID     = var.DINNER_DONE_BETTER_OAUTH2_CLIENT_ID
     DINNER_DONE_BETTER_OAUTH2_CLIENT_SECRET = var.DINNER_DONE_BETTER_OAUTH2_CLIENT_SECRET
   }
 }
