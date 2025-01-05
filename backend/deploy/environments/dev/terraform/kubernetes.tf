f377a655fab592cb21f2449f49a5b4246b1bb748
Jeffrey D. <verygoodsoftwarenotvirus@protonmail.com>
2025-01-05 00:38:25 -0600

    let's just re-do cert manager

--- backend/deploy/environments/dev/terraform/kubernetes.tf
+++ backend/deploy/environments/dev/terraform/kubernetes.tf
@@ -98,24 +98,3 @@ resource "kubernetes_secret" "api_service_config" {
     GRAFANA_CLOUD_TEMPO_PASSWORD      = var.GRAFANA_CLOUD_TEMPO_PASSWORD
   }
 }
-
-resource "kubernetes_secret" "https_certificate" {
-  metadata {
-    name      = "tls-certs"
-    namespace = local.k8s_namespace
-
-    annotations = {
-      (local.managed_by_label) = "terraform"
-    }
-
-    labels = {
-      (local.managed_by_label) = "terraform"
-    }
-  }
-
-  depends_on = [data.google_container_cluster.dev_cluster]
-
-  data = {
-    cert: jsonencode(data.google_certificate_manager_certificates.dev),
-  }
-}
--- infra/deploy/environments/dev/terraform/networking.tf
+++ infra/deploy/environments/dev/terraform/networking.tf
@@ -4,6 +4,13 @@ resource "google_compute_network" "private_network" {
   name = "private-network"
 }
 
+resource "google_compute_address" "static_ip" {
+  name = "dev"
+  labels = {
+    (local.managed_by_label) = "terraform"
+  }
+}
+
 resource "google_compute_global_address" "private_ip_address" {
   provider = google
 
@@ -20,87 +27,4 @@ resource "google_service_networking_connection" "private_vpc_connection" {
   network                 = google_compute_network.private_network.id
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
-}
-
-resource "google_compute_address" "static_ip" {
-  name = "dev"
-  labels = {
-    (local.managed_by_label) = "terraform"
-  }
-}
-
-resource "google_certificate_manager_dns_authorization" "api" {
-  name        = "api"
-  domain      = "api.dinnerdonebetter.dev"
-}
-
-resource "google_certificate_manager_dns_authorization" "admin" {
-  name        = "admin"
-  domain      = "admin.dinnerdonebetter.dev"
-}
-
-resource "google_certificate_manager_dns_authorization" "app" {
-  name        = "app"
-  domain      = "app.dinnerdonebetter.dev"
-}
-
-resource "google_certificate_manager_dns_authorization" "www" {
-  name   = "www"
-  domain = "www.dinnerdonebetter.dev"
-}
-
-resource "google_certificate_manager_dns_authorization" "media" {
-  name   = "media"
-  domain = "media.dinnerdonebetter.dev"
-}
-
-resource "google_certificate_manager_dns_authorization" "userdata" {
-  name   = "userdata"
-  domain = "userdata.dinnerdonebetter.dev"
-}
-
-resource "google_certificate_manager_certificate" "default" {
-  name        = "dns-cert"
-  description = "The default cert"
-  scope       = "EDGE_CACHE"
-  labels = {
-    (local.managed_by_label) = "terraform"
-  }
-  managed {
-    domains = [
-      google_certificate_manager_dns_authorization.api.domain,
-      google_certificate_manager_dns_authorization.admin.domain,
-      google_certificate_manager_dns_authorization.app.domain,
-      google_certificate_manager_dns_authorization.www.domain,
-      google_certificate_manager_dns_authorization.media.domain,
-      google_certificate_manager_dns_authorization.userdata.domain,
-    ]
-    dns_authorizations = [
-      google_certificate_manager_dns_authorization.api.id,
-      google_certificate_manager_dns_authorization.admin.id,
-      google_certificate_manager_dns_authorization.app.id,
-      google_certificate_manager_dns_authorization.www.id,
-      google_certificate_manager_dns_authorization.media.id,
-      google_certificate_manager_dns_authorization.userdata.id,
-    ]
-  }
-}
-
-resource "cloudflare_record" "cert_domain_verification_records" {
-  for_each = {
-    "0": google_certificate_manager_dns_authorization.api.dns_resource_record.0
-    "1": google_certificate_manager_dns_authorization.admin.dns_resource_record.0
-    "2": google_certificate_manager_dns_authorization.app.dns_resource_record.0
-    "3": google_certificate_manager_dns_authorization.www.dns_resource_record.0
-    "4": google_certificate_manager_dns_authorization.media.dns_resource_record.0
-    "5": google_certificate_manager_dns_authorization.userdata.dns_resource_record.0
-  }
-
-  zone_id  = var.CLOUDFLARE_ZONE_ID
-  name     = each.value.name
-  type     = upper(each.value.type)
-  content  = each.value.data
-  ttl      = 1
-  proxied  = false
-  comment  = "Managed by Terraform"
 }
