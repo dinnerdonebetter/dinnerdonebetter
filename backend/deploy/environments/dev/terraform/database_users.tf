7d980c1aef72960bfef6d19e516c5a3bf5049927
Jeffrey D. <verygoodsoftwarenotvirus@protonmail.com>
2025-01-18 20:01:27 -0600

    change things so that terraform runs

--- backend/deploy/environments/dev/terraform/database_users.tf
+++ backend/deploy/environments/dev/terraform/database_users.tf
@@ -13,3 +13,15 @@ resource "google_sql_user" "api_user" {
   instance = google_sql_database_instance.dev.name
   password = random_password.api_user_database_password.result
 }
+
+resource "random_password" "meal_plan_finalizer_user_database_password" {
+  length           = 64
+  special          = true
+  override_special = "#$*-_=+[]"
+}
+
+resource "google_sql_user" "meal_plan_finalizer_user" {
+  name     = "meal_plan_finalizer"
+  instance = google_sql_database_instance.dev.name
+  password = random_password.api_user_database_password.result
+}
