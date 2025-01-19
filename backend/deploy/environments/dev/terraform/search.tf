b82342ef41af0f6b0245f43dd870b124eeaf21c9
Jeffrey D. <verygoodsoftwarenotvirus@protonmail.com>
2025-01-18 22:39:02 -0600

    try this?

--- backend/deploy/environments/dev/terraform/search.tf
+++ backend/deploy/environments/dev/terraform/search.tf
@@ -2,76 +2,32 @@
 
 resource "algolia_index" "recipes_index" {
   name = "recipes"
-
-  searchable_attributes = [
-    "name",
-    "description",
-  ]
 }
 
 resource "algolia_index" "meals_index" {
   name = "meals"
-
-  searchable_attributes = [
-    "name",
-    "description",
-  ]
 }
 
 resource "algolia_index" "valid_ingredients_index" {
   name = "valid_ingredients"
-
-  searchable_attributes = [
-    "name",
-    "pluralName",
-    "description",
-  ]
 }
 
 resource "algolia_index" "valid_instruments_index" {
   name = "valid_instruments"
-
-  searchable_attributes = [
-    "name",
-    "pluralName",
-    "description",
-  ]
 }
 
 resource "algolia_index" "valid_measurement_units_index" {
   name = "valid_measurement_units"
-
-  searchable_attributes = [
-    "name",
-    "pluralName",
-    "description",
-  ]
 }
 
 resource "algolia_index" "valid_preparations_index" {
   name = "valid_preparations"
-
-  searchable_attributes = [
-    "name",
-    "pastTense",
-    "description",
-  ]
 }
 
 resource "algolia_index" "valid_ingredient_states_index" {
   name = "valid_ingredient_states"
-
-  searchable_attributes = [
-    "name",
-    "pastTense",
-    "description",
-  ]
 }
 
 resource "algolia_index" "users_index" {
   name = "users"
-
-  searchable_attributes = [
-    "username",
-  ]
 }
