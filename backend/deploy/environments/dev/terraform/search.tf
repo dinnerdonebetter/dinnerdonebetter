5df5c3cab052a097d7ee64e717ed1481e8c92e2d
Jeffrey D. <verygoodsoftwarenotvirus@protonmail.com>
2025-01-19 01:43:16 -0600

    fix search I think?

--- backend/deploy/environments/dev/terraform/search.tf
+++ backend/deploy/environments/dev/terraform/search.tf
@@ -1,33 +1,75 @@
-# how to use these resources: https://github.com/philippe-vandermoere/terraform-provider-algolia/blob/11d9e162be54c66c92376ae5647f7f3bd675755a/examples/main.tf
+resource "algolia_index" "recipes_index" {
+  name = "recipes"
 
-# resource "algolia_index" "recipes_index" {
-#   name = "dev_recipes"
-# }
+  searchable_attributes = [
+    "name",
+    "description",
+  ]
+}
 
-# resource "algolia_index" "meals_index" {
-#   name = "dev_meals"
-# }
+resource "algolia_index" "meals_index" {
+  name = "meals"
 
-# resource "algolia_index" "valid_ingredients_index" {
-#   name = "dev_valid_ingredients"
-# }
+  searchable_attributes = [
+    "name",
+    "description",
+  ]
+}
 
-# resource "algolia_index" "valid_instruments_index" {
-#   name = "dev_valid_instruments"
-# }
+resource "algolia_index" "valid_ingredients_index" {
+  name = "valid_ingredients"
 
-# resource "algolia_index" "valid_measurement_units_index" {
-#   name = "dev_valid_measurement_units"
-# }
+  searchable_attributes = [
+    "name",
+    "pluralName",
+    "description",
+  ]
+}
 
-# resource "algolia_index" "valid_preparations_index" {
-#   name = "dev_valid_preparations"
-# }
+resource "algolia_index" "valid_instruments_index" {
+  name = "valid_instruments"
 
-# resource "algolia_index" "valid_ingredient_states_index" {
-#   name = "dev_valid_ingredient_states"
-# }
+  searchable_attributes = [
+    "name",
+    "pluralName",
+    "description",
+  ]
+}
 
-# resource "algolia_index" "users_index" {
-#   name = "dev_users"
-# }
+resource "algolia_index" "valid_measurement_units_index" {
+  name = "valid_measurement_units"
+
+  searchable_attributes = [
+    "name",
+    "pluralName",
+    "description",
+  ]
+}
+
+resource "algolia_index" "valid_preparations_index" {
+  name = "valid_preparations"
+
+  searchable_attributes = [
+    "name",
+    "pastTense",
+    "description",
+  ]
+}
+
+resource "algolia_index" "valid_ingredient_states_index" {
+  name = "valid_ingredient_states"
+
+  searchable_attributes = [
+    "name",
+    "pastTense",
+    "description",
+  ]
+}
+
+resource "algolia_index" "users_index" {
+  name = "users"
+
+  searchable_attributes = [
+    "username",
+  ]
+}
