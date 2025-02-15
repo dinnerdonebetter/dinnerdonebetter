4d3c3984bba824ff4682bc2569afcb23cb1ed028
Jeffrey D <1289344+verygoodsoftwarenotvirus@users.noreply.github.com>
2025-02-14 21:52:08 -0600

    rewire

--- backend/internal/build/jobs/db_cleaner/wire_gen.go
+++ backend/internal/build/jobs/db_cleaner/wire_gen.go
@@ -12,10 +12,10 @@ import (
 	"github.com/dinnerdonebetter/backend/internal/config"
 	"github.com/dinnerdonebetter/backend/internal/database"
 	"github.com/dinnerdonebetter/backend/internal/database/postgres"
-	loggingcfg "github.com/dinnerdonebetter/backend/internal/lib/observability/logging/config"
-	metricscfg "github.com/dinnerdonebetter/backend/internal/lib/observability/metrics/config"
-	tracingcfg "github.com/dinnerdonebetter/backend/internal/lib/observability/tracing/config"
-	dbcleaner "github.com/dinnerdonebetter/backend/internal/services/core/workers/db_cleaner"
+	"github.com/dinnerdonebetter/backend/internal/lib/observability/logging/config"
+	"github.com/dinnerdonebetter/backend/internal/lib/observability/metrics/config"
+	"github.com/dinnerdonebetter/backend/internal/lib/observability/tracing/config"
+	"github.com/dinnerdonebetter/backend/internal/services/core/workers/db_cleaner"
 )
 
 // Injectors from build.go:
