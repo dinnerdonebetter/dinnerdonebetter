e01b5947604f4039da45071c962a5a6d0601c4f1
Jeffrey D <1289344+verygoodsoftwarenotvirus@users.noreply.github.com>
2025-02-23 18:04:35 -0600

    fix build

---
+++ backend/tests/integration/doc.go
@@ -0,0 +1,4 @@
+/*
+Package integration is a series of tests which utilize our client to talk to a running instance of the server to validate behaviors, inputs, and outputs.
+*/
+package integration
