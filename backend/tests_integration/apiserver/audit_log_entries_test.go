3c0047050fafa9be84cd1531cbdd098234635e9a
Jeffrey D <1289344+verygoodsoftwarenotvirus@users.noreply.github.com>
2025-09-22 23:53:13 -0500

    obligatory change to trigger lint

---
+++ backend/tests_integration/apiserver/audit_log_entries_test.go
@@ -0,0 +1,52 @@
+package integration
+
+import (
+	"context"
+	"testing"
+
+	auditgrpc "github.com/dinnerdonebetter/backend/internal/grpc/generated/services/audit"
+	"github.com/dinnerdonebetter/backend/internal/platform/observability/tracing"
+	"github.com/stretchr/testify/require"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAuditLogEntries_Listing(T *testing.T) {
+	T.Parallel()
+
+	T.Run("should be able to be read in a list", func(t *testing.T) {
+		t.SkipNow()
+		ctx := t.Context()
+
+		_, userClient := createUserAndClientForTest(t)
+
+		forUser, err := userClient.GetAuditLogEntriesForUser(ctx, &auditgrpc.GetAuditLogEntriesForUserRequest{})
+		require.NoError(t, err)
+
+		assert.Equal(t, 4, len(forUser.Results))
+
+		forAccount, err := userClient.GetAuditLogEntriesForAccount(ctx, &auditgrpc.GetAuditLogEntriesForAccountRequest{})
+		require.NoError(t, err)
+
+		assert.Equal(t, 2, len(forAccount.Results))
+	})
+}
+
+/*
+
+func (s *TestSuite) TestWebhooks_Retrieving_Returns404ForNonexistentAuditLogEntry() {
+	s.runTest("should error when archiving a non-existent auditLogEntry", func(testClients *testClientWrapper) func() {
+		return func() {
+			t := s.T()
+
+			ctx, span := tracing.StartCustomSpan(s.ctx, t.Name())
+			defer span.End()
+
+			actual, err := testClients.userClient.GetAuditLogEntryByID(ctx, nonexistentID)
+			assert.Nil(t, actual)
+			assert.Error(t, err)
+		}
+	})
+}
+
+*/
