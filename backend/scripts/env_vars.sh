1142fcf6c93d99417ca25f3fe989f9aab4995c4e
Jeffrey D <1289344+verygoodsoftwarenotvirus@users.noreply.github.com>
2026-01-01 13:22:42 -0600

    fix env vars file

--- backend/scripts/env_vars.sh
+++ backend/scripts/env_vars.sh
@@ -8,6 +8,3 @@ PACKAGE_PREFIX="${1:-github.com/dinnerdonebetter/backend}"
 GO_FORMAT="${2:-gofmt -s -w}"
 
 go run "${PACKAGE_PREFIX}/cmd/tools/codegen/valid_env_vars"
-# shellcheck disable=SC2086
-${GO_FORMAT} internal/config/envvars/*.go
-
