c228cfe7dd514b3a26c1239d3cd7be71f391389a
Jeffrey D <1289344+verygoodsoftwarenotvirus@users.noreply.github.com>
2026-03-14 23:44:18 -0500

    fix markdown

--- scripts/lint_markdown.sh
+++ scripts/lint_markdown.sh
@@ -6,4 +6,4 @@ set -euo pipefail
 
 CONTAINER_RUNNER="${1:-docker}"
 
-"${CONTAINER_RUNNER}" run --rm --volume $PWD:$PWD --workdir=$PWD --user $(id -u):$(id -g) ghcr.io/igorshubovych/markdownlint-cli:latest --ignore "**/vendor/**" --ignore "**/ios/build/**" "**/*.md" --fix --disable=MD013
+"${CONTAINER_RUNNER}" run --rm --volume $PWD:$PWD --workdir=$PWD --user $(id -u):$(id -g) ghcr.io/igorshubovych/markdownlint-cli:latest --ignore "**/vendor/**" --ignore "**/node_modules/**" --ignore "**/ios/build/**" "**/*.md" --fix --disable=MD013
